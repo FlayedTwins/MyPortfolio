cb7803af96e9498e172116bd024b1ed37eef516f
FlayedTwins <theflayedtwins16@gmail.com>
2022-08-30 15:53:38 +0400

    6th Commit

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -625,3 +625,13 @@
 
 
 })(jQuery);
+
+
+$(".images img").click(function(){
+	$("#full-image").attr("src", $(this).attr("src"));
+	$('#image-viewer').show();
+  });
+  
+  $("#image-viewer .close").click(function(){
+	$('#image-viewer').hide();
+  });
